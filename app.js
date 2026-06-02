358609b4f24fb93594c9f84e1cf5a83a0010ad35
TarunUk <tarunrajput.8606@gmail.com>
2026-06-02 14:33:22 +0530

    updated project

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Jenkins CI/CD Working Successfully!');
+app.use(express.static("public"));
+
+app.get("/api/message", (req, res) => {
+    res.json({ message: "🚀 Hello from Backend API!" });
 });
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+    console.log("Server running on port 3000");
 });
